c8e7f9c6df0faebab8bfc0c3acecc21ea49cf202
Andrew Burns <burnsad@tcd.ie>
2020-06-26 11:59:46 +0000

    promise jquery messages added via functions

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -6,5 +6,19 @@ function fetchGitHubInformation(event){
     }
     $("#gh-user-data").html(`
     <div id="loader"><img src="assets/css/loader.gif" alt="loading.."></div>`);
-$.when
+
+    $.when(
+    $.getJSON(`https://api.github.com/users${username}`)
+).then(
+    function (response){
+        var userData = response;
+        $("#gh-user-data").html(userinformationHTML(userData));
+    }, function(errorResponse){
+        if (errorResponse.status === 404){
+            $ ("#gh-user-data").html(`<h2>No info found for this Fine Gael user ${username}</h2>`);
+        } else { 
+        console.log (errorResponse);
+        $("#gh-user-data").html(`<h2> Error: ${errorResponse.responseJSON.message}</h2>`);
+    }
+}); 
 }
