07932de1642e6db065a31e8041183263118b0f2a
Andrew Burns <burnsad@tcd.ie>
2020-07-08 18:40:44 +0000

    sendEmail js not working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie",{
+        "from_name": contactForm.name.value, 
+        "from_email":contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("Success", response);
+        },
+        function(error){
+           console.log("failed", error) 
+        });
+        return false;
+}
